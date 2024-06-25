d669bfbd5316eebe7eca410f2bb4d11e0e87255f
“maitreverge_42” <verge.f@gmail.com>
2024-06-25 11:31:24 +0200

    merci Lous

--- graph_part.c
+++ graph_part.c
@@ -6,7 +6,7 @@
 /*   By: flverge <flverge@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 21:24:31 by iarrar            #+#    #+#             */
-/*   Updated: 2024/06/25 10:57:00 by flverge          ###   ########.fr       */
+/*   Updated: 2024/06/25 11:24:28 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,28 +36,28 @@ void	ft_init_img(t_data *data)
 		(data->mlx_ptr, "./img/black.xpm", &img_height, &img_width);
 
 	data->enemy.sprite.mlx_img = mlx_xpm_file_to_image
-		(data->mlx_ptr, "./img/loup.xpm", &img_height, &img_width);
+		(data->mlx_ptr, "./img/oeuf.xpm", &img_height, &img_width);
 
 	data->player_2.sprite.mlx_img = mlx_xpm_file_to_image
-		(data->mlx_ptr, "./img/mouton.xpm", &img_height, &img_width);
+		(data->mlx_ptr, "./img/singedrole2.xpm", &img_height, &img_width);
 	
 	data->player.sprite.mlx_img = mlx_xpm_file_to_image
-		(data->mlx_ptr, "./img/right_boo.xpm", &img_height, &img_width);
+		(data->mlx_ptr, "./img/boo.xpm", &img_height, &img_width); // boo de Lou
 		
 	data->wall.sprite.mlx_img = mlx_xpm_file_to_image
-		(data->mlx_ptr, "./img/rideau.xpm", &img_height, &img_width);
+		(data->mlx_ptr, "./img/wall_2.xpm", &img_height, &img_width);
 		
 	data->font.sprite.mlx_img = mlx_xpm_file_to_image
-		(data->mlx_ptr, "./img/floor.xpm", &img_height, &img_width);
+		(data->mlx_ptr, "./img/newsol.xpm", &img_height, &img_width);
 		
 	data->exit_open.sprite.mlx_img = mlx_xpm_file_to_image
-		(data->mlx_ptr, "./img/exit_open.xpm", &img_height, &img_width);
+		(data->mlx_ptr, "./img/sortiegrotte.xpm", &img_height, &img_width);
 		
 	data->exit_close.sprite.mlx_img = mlx_xpm_file_to_image
-		(data->mlx_ptr, "./img/exit_close.xpm", &img_height, &img_width);
+		(data->mlx_ptr, "./img/grotteourse.xpm", &img_height, &img_width);
 		
 	data->cookie.sprite.mlx_img = mlx_xpm_file_to_image
-		(data->mlx_ptr, "./img/licorn.xpm", &img_height, &img_width);
+		(data->mlx_ptr, "./img/bigmac.xpm", &img_height, &img_width);
 }
 
 void	put_image(t_data *data)
